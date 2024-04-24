ce72f69a4aa0205e6673561a98741fac2812189e
maxix28 <maxyxpavliv@gmail.com>
2024-04-24 16:53:42 +0300

    separate files

--- app/src/main/java/com/example/talkify/utils/ItemsList.kt
+++ app/src/main/java/com/example/talkify/utils/ItemsList.kt
@@ -5,7 +5,7 @@ import com.example.talkify.R
 
 val items = listOf<Item>(
 
-    Item("1", R.drawable.),
+    Item("1", R.drawable.img_1),
     Item("1", R.drawable.img_2),
     Item("1", R.drawable.img_3),
     Item("1", R.drawable.img_4),
@@ -16,7 +16,7 @@ val items = listOf<Item>(
     Item("1", R.drawable.img_4),
     Item("1", R.drawable.img_5),
     Item("1", R.drawable.img_6),
-    Item("1", R.drawable.img),
+    Item("1", R.drawable.img_1),
     Item("1", R.drawable.img_2),
     Item("1", R.drawable.img_3),
     Item("1", R.drawable.img_4),
@@ -27,7 +27,7 @@ val items = listOf<Item>(
     Item("1", R.drawable.img_4),
     Item("1", R.drawable.img_5),
     Item("1", R.drawable.img_6),
-    Item("1", R.drawable.img),
+    Item("1", R.drawable.img_1),
     Item("1", R.drawable.img_2),
     Item("1", R.drawable.img_3),
     Item("1", R.drawable.img_4),
