956c7e10630f79cf945aa55bb16e5e823ca9a715
AsketRidik <33568717+AsketRidik@users.noreply.github.com>
2024-05-06 17:18:16 +0300

    settings colors and size

--- app/src/main/java/com/example/talkify/ui/main_screen/composable/Slider.kt
+++ app/src/main/java/com/example/talkify/ui/main_screen/composable/Slider.kt
@@ -3,11 +3,16 @@ package com.example.talkify.ui.main_screen.composable
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
+val activeTrackColor = Color(0xFFF6891E) // orange
+val inactiveTrackColor = Color(0xFFA9A5A5) // Grey
+val thumbColor = Color(0xFFEC417A) // pink
 @Composable
 fun SettingsSlider(sliderValue: MutableState<Float>,
                    onValueChange: (Float) -> Unit) {
@@ -18,7 +23,12 @@ fun SettingsSlider(sliderValue: MutableState<Float>,
                 sliderValue.value = newValue
                 onValueChange(newValue)
             },
-            valueRange = 0f..100f
+            valueRange = 0f..100f,
+            colors = SliderDefaults.colors(
+            activeTrackColor = activeTrackColor,
+            inactiveTrackColor = inactiveTrackColor,
+            thumbColor = thumbColor
+            )
         )
     }
 }
