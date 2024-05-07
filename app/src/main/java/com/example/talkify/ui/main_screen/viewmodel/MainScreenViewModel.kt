303507f284b2219c56ee053a1834e4e0431fab2d
maxix28 <123401537+maxix28@users.noreply.github.com>
2024-05-07 13:38:06 +0300

    minor change

--- app/src/main/java/com/example/talkify/ui/main_screen/viewmodel/MainScreenViewModel.kt
+++ app/src/main/java/com/example/talkify/ui/main_screen/viewmodel/MainScreenViewModel.kt
@@ -17,47 +17,50 @@ import javax.inject.Inject
 @HiltViewModel
 class MainScreenViewModel @Inject constructor(
     private val makeListUseCase: MakeListUseCase
-    ) : ViewModel() {
+) : ViewModel() {
     private var _uiState: MainScreenState by mutableStateOf(MainScreenState())
         private set
     val uiState: MainScreenState
         get() = _uiState
 
-val category ="Fruits"
+    val category = "Fruits"
 
     fun onEvent(state: MainScreenStates) {
         when (state) {
             MainScreenStates.Edit -> editScreen()
             is MainScreenStates.ChangeList -> {
                 viewModelScope.launch {
-                    updateUIState(  makeListUseCase( state.listName))
+                    updateUIState(makeListUseCase(state.listName))
                 }
             }
+
             MainScreenStates.GoHome -> TODO()
             MainScreenStates.OpenSetting -> TODO()
 
 
         }
     }
-    init{
+
+    init {
         viewModelScope.launch {
             try {
-                updateUIState( makeListUseCase(category))
-            }
-            catch (e:Exception){//  при першому створенні бази даних не встигають створитися категорії
+                updateUIState(makeListUseCase(category))
+            } catch (e: Exception) {//  при першому створенні бази даних не встигають створитися категорії
                 delay(8000)
-               updateUIState( makeListUseCase(category))
+                updateUIState(makeListUseCase(category))
 
             }
         }
     }
+
     private fun editScreen() {
         val current = _uiState.edit
         val updated = _uiState.copy(edit = !current)
         _uiState = updated
 
     }
-    private fun updateUIState(itemList:List<ItemUI>) {
-        _uiState = _uiState.copy(list = itemList ?: emptyList())
+
+    private fun updateUIState(itemList: List<ItemUI>) {
+        _uiState = _uiState.copy(list = itemList)
     }
 }
