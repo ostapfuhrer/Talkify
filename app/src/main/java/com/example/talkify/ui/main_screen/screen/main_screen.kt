42a18fb4d7da23989ad0a10b349e902a841ab73d
AsketRidik <33568717+AsketRidik@users.noreply.github.com>
2024-05-08 12:32:09 +0300

    Update main_screen.kt

--- app/src/main/java/com/example/talkify/ui/main_screen/screen/main_screen.kt
+++ app/src/main/java/com/example/talkify/ui/main_screen/screen/main_screen.kt
@@ -19,7 +19,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.talkify.R
